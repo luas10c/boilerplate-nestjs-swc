ffcd0abd6822f313ad6d30b6f96a93071bfae9bf
Luciano Alves <luas10c@gmail.com>
2024-11-28 03:51:44 -0300

    chore: first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+const config = {
+  root: true,
+  env: {
+    es2022: true,
+    node: true
+  },
+  extends: ['prettier'],
+  plugins: ['prettier', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 13
+  },
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
+
+module.exports = config
