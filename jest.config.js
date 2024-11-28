ba7a047185fc8dbe22395020b4c29f95cb40f4f1
Luciano Alves <luas10c@gmail.com>
2024-11-28 03:51:44 -0300

    chore: first commit

---
+++ jest.config.js
@@ -0,0 +1,36 @@
+/** @type{import('jest').Config} */
+const config = {
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          baseUrl: '.',
+          parser: {
+            syntax: 'typescript',
+            decorators: true
+          },
+          target: 'es2022',
+          keepClassNames: true,
+          transform: {
+            decoratorMetadata: true,
+            legacyDecorator: true
+          },
+          paths: {
+            '#/*': ['./src/*']
+          }
+        },
+        module: {
+          type: 'es6',
+          strict: true,
+          noInterop: true
+        }
+      }
+    ]
+  }
+}
+
+export default config
